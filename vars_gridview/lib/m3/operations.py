c427e86e6197f8c45d8fb6a4613a8e8a8300e59f
Kevin Barnard <kbarnard@mbari.org>
2023-02-17 13:01:41 -0800

    Add logging to operations, support new endpoints

--- vars_gridview/lib/m3/operations.py
+++ vars_gridview/lib/m3/operations.py
@@ -9,6 +9,7 @@ from typing import List
 import requests
 
 from vars_gridview.lib import m3
+from vars_gridview.lib.log import LOGGER
 
 KB_CONCEPTS = None
 KB_PARTS = None
@@ -21,10 +22,12 @@ def get_kb_concepts() -> List[str]:
     """
     global KB_CONCEPTS
     if not KB_CONCEPTS:
+        LOGGER.debug("Getting concepts from KB")
         response = m3.VARS_KB_SERVER_CLIENT.get_concepts()
 
         response.raise_for_status()
         KB_CONCEPTS = response.json()
+        LOGGER.debug(f"Got {len(KB_CONCEPTS)} concepts from KB")
 
     return KB_CONCEPTS
 
@@ -35,10 +38,12 @@ def get_kb_parts() -> List[str]:
     """
     global KB_PARTS
     if not KB_PARTS:
+        LOGGER.debug("Getting parts from KB")
         response = m3.VARS_KB_SERVER_CLIENT.get_parts()
 
         response.raise_for_status()
         KB_PARTS = [part["name"] for part in response.json()]
+        LOGGER.debug(f"Got {len(KB_PARTS)} parts from KB")
 
     return KB_PARTS
 
@@ -47,6 +52,7 @@ def get_kb_descendants(concept: str) -> List[str]:
     """
     Get a list of all descendants of a concept in the KB, including the concept.
     """
+    LOGGER.debug(f"Getting descendants of {concept} from KB")
     response = m3.VARS_KB_SERVER_CLIENT.get_phylogeny_taxa(concept)
 
     if response.status_code == 404:
@@ -55,7 +61,9 @@ def get_kb_descendants(concept: str) -> List[str]:
         response.raise_for_status()
 
     parsed_response = response.json()
-    return [taxa["name"] for taxa in parsed_response]
+    taxa_names = [taxa["name"] for taxa in parsed_response]
+    LOGGER.debug(f"Got {len(taxa_names)} descendants of {concept} from KB")
+    return taxa_names
 
 
 def get_users() -> List[dict]:
@@ -64,10 +72,12 @@ def get_users() -> List[dict]:
     """
     global USERS
     if not USERS:
+        LOGGER.debug("Getting users from VARS user server")
         response = m3.VARS_USER_SERVER_CLIENT.get_all_users()
 
         response.raise_for_status()
         USERS = response.json()
+        LOGGER.debug(f"Got {len(USERS)} users from VARS user server")
 
     return USERS
 
@@ -80,6 +90,7 @@ def update_bounding_box_data(association_uuid: str, box_dict: dict) -> dict:
         "link_value": json.dumps(box_dict),
     }
 
+    LOGGER.debug(f"Updating bounding box data for {association_uuid}:\n{request_data}")
     response = m3.ANNOSAURUS_CLIENT.update_association(association_uuid, request_data)
     response.raise_for_status()
     return response.json()
@@ -93,6 +104,7 @@ def update_bounding_box_part(association_uuid: str, part: str) -> dict:
         "to_concept": part,
     }
 
+    LOGGER.debug(f"Updating bounding box part for {association_uuid}:\n{request_data}")
     response = m3.ANNOSAURUS_CLIENT.update_association(association_uuid, request_data)
     response.raise_for_status()
     return response.json()
@@ -109,18 +121,69 @@ def update_observation_concept(
         "observer": observer,  # when we update the concept, we also update the observer
     }
 
+    LOGGER.debug(f"Updating observation concept for {observation_uuid}:\n{request_data}")
     response = m3.ANNOSAURUS_CLIENT.update_observation(observation_uuid, request_data)
 
     response.raise_for_status()
     return response.json()
 
 
-def delete_bounding_box(association_uuid: str):
+def create_association(association: dict) -> requests.Response:
     """
-    Delete a bounding box.
+    Create an association.
     """
+    LOGGER.debug(f"Creating association:\n{association}")
+    response = m3.ANNOSAURUS_CLIENT.create_association(association)
+
+    response.raise_for_status()
+    return response
+
+
+def delete_association(association_uuid: str):
+    """
+    Delete an association.
+    
+    Args:
+        association_uuid: UUID of the association to delete.
+    """
+    LOGGER.debug(f"Deleting association {association_uuid}")
     response = m3.ANNOSAURUS_CLIENT.delete_association(association_uuid)
 
+    response.raise_for_status()
+
+
+def get_observation(observation_uuid: str) -> requests.Response:
+    """
+    Get an observation by UUID.
+    """
+    LOGGER.debug(f"Getting observation {observation_uuid}")
+    response = m3.ANNOSAURUS_CLIENT.get_observation(observation_uuid)
+    
+    response.raise_for_status()
+    return response.json()
+
+
+def create_observation(observation: dict) -> requests.Response:
+    """
+    Create an observation.
+    """
+    LOGGER.debug(f"Creating observation:\n{observation}")
+    response = m3.ANNOSAURUS_CLIENT.create_observation(observation)
+
+    response.raise_for_status()
+    return response
+
+
+def delete_observation(observation_uuid: str):
+    """
+    Delete an observation.
+    
+    Args:
+        observation_uuid: UUID of the observation to delete.
+    """
+    LOGGER.debug(f"Deleting observation {observation_uuid}")
+    response = m3.ANNOSAURUS_CLIENT.delete_observation(observation_uuid)
+    
     response.raise_for_status()
 
 
@@ -130,6 +193,7 @@ def get_videos_at_datetime(dt: datetime) -> List[dict]:
     """
     timestamp = dt.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
 
+    LOGGER.debug(f"Getting videos at {timestamp}")
     response = m3.VAMPIRE_SQUID_CLIENT.get_videos_at_timestamp(timestamp)
 
     response.raise_for_status()
@@ -141,6 +205,7 @@ def get_vars_imaged_moment(image_reference_uuid: str) -> dict:
     Get MBARI VARS imaged moment by UUID.
     This will not work unless you are connected to the MBARI network!
     """
+    LOGGER.debug(f"Getting VARS imaged moment {image_reference_uuid}")
     response = requests.get(
         "http://m3.shore.mbari.org/anno/v1/imagedmoments/" + image_reference_uuid
     )
