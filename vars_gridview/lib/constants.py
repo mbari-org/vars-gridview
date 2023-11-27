2f6252c3e7f784f1c32fc78a5f30efbf330d6eb4
Kevin Barnard <kbarnard@mbari.org>
2023-11-27 12:26:29 -0800

    Add user log directory path

--- vars_gridview/lib/constants.py
+++ vars_gridview/lib/constants.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from platformdirs import user_log_dir
 
 # Application
 APP_NAME = "VARS GridView"
@@ -22,7 +23,7 @@ UI_FILE = ASSETS_DIR / "gridview.ui"
 STYLE_DIR = ASSETS_DIR / "style"
 GUI_SETTINGS_FILE = ASSETS_DIR / "gui.ini"
 BASE_QUERY_FILE = ASSETS_DIR / "base_query.sql"
-LOG_DIR = ROOT_DIR / "logs"
+LOG_DIR = Path(user_log_dir(APP_NAME, APP_ORGANIZATION))
 
 # Preferred image type
 IMAGE_TYPE = "image/png"
