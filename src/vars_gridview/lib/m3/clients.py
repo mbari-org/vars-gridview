86a52e00677b5d6700cfae3733e35c9e86c7316f
Kevin Barnard <kbarnard@mbari.org>
2025-10-22 11:08:23 -0700

    fix: add reauthentication decorator to handle JWT expiry
    #66

--- src/vars_gridview/lib/m3/clients.py
+++ src/vars_gridview/lib/m3/clients.py
@@ -53,22 +53,52 @@ def needs_auth(
     return wrapper
 
 
+def reauth(
+    f: Callable[Concatenate["M3Client", P], R],
+) -> Callable[Concatenate["M3Client", P], R]:
+    """
+    Decorator to re-authenticate and retry the function if it fails due to not being authenticated.
+    """
+
+    @wraps(f)
+    def wrapper(self: M3Client, *args: P.args, **kwargs: P.kwargs) -> R:
+        try:
+            return f(self, *args, **kwargs)
+        except NotAuthenticated:
+            self.authenticate()
+            return f(self, *args, **kwargs)
+        except requests.HTTPError as e:
+            if e.response.status_code in (401, 403):
+                self.authenticate()
+                return f(self, *args, **kwargs)
+            else:
+                raise e
+
+    return wrapper
+
+
 class M3Client:
     """
     M3 microservice client.
     """
 
-    def __init__(self, base_url: str, api_key: str = None):
+    def __init__(self, base_url: str, api_key: Optional[str] = None):
         self._session = requests.Session()
 
         self.base_url = base_url
 
+        self._api_key = None
         if api_key is not None:
+            self._api_key = api_key
             self.authenticate(api_key)
 
     @property
     def base_url(self) -> str:
         return self._base_url
+
+    @property
+    def api_key(self) -> Optional[str]:
+        return self._api_key
 
     @base_url.setter
     def base_url(self, base_url: str):
@@ -99,11 +129,20 @@ class M3Client:
         """
         return self._session.auth is not None
 
-    def authenticate(self, api_key: str, auth_path: str = "/auth"):
+    def authenticate(self, api_key: Optional[str] = None, auth_path: str = "/auth"):
         """
         Authenticate the client with the provided API key.
+
+        Args:
+            api_key (Optional[str]): The API key to use for authentication. If None, uses the API key provided at initialization.
+            auth_path (str): The authentication path.
         """
         self._session.auth = None
+
+        if api_key is None:
+            api_key = self._api_key
+        if api_key is None:
+            raise ValueError("API key must be provided for authentication")
 
         response = self.post(auth_path, headers={"Authorization": f"APIKEY {api_key}"})
         response.raise_for_status()
@@ -117,16 +156,19 @@ class AnnosaurusClient(M3Client):
     Annosaurus (v1) client.
     """
 
+    @reauth
     @needs_auth
     def create_association(self, data: dict) -> requests.Response:
         return self.post("/associations", data=data)
 
+    @reauth
     @needs_auth
     def update_association(
         self, association_uuid: str, data: dict
     ) -> requests.Response:
         return self.put(f"/associations/{association_uuid}", data=data)
 
+    @reauth
     @needs_auth
     def delete_association(self, association_uuid: str) -> requests.Response:
         return self.delete(f"/associations/{association_uuid}")
@@ -134,16 +176,19 @@ class AnnosaurusClient(M3Client):
     def get_observation(self, observation_uuid: str) -> requests.Response:
         return self.get(f"/observations/{observation_uuid}")
 
+    @reauth
     @needs_auth
     def create_observation(self, data: dict) -> requests.Response:
         return self.post("/annotations", data=data)
 
+    @reauth
     @needs_auth
     def update_observation(
         self, observation_uuid: str, data: dict
     ) -> requests.Response:
         return self.put(f"/observations/{observation_uuid}", data=data)
 
+    @reauth
     @needs_auth
     def delete_observation(self, observation_uuid: str) -> requests.Response:
         return self.delete(f"/observations/{observation_uuid}")
